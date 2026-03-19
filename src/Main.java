f78f7a4be6e1e349ea40e4b7fef8cb5ae78aac36
anuragpatwari591-hub <anuragpatwari591@gmail.com>
2026-03-19 14:52:15 +0530

    Updated Main class to test library functionality

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Library Management System Started");
+        LibraryManager lib = new LibraryManager();
 
+        // Adding sample book
+        Book b1 = new FictionBook(1, "Harry Potter", "J.K Rowling");
+        lib.addBook(b1);
+
+        // Adding sample user
+        User u1 = new Student(101, "Anurag");
+        lib.addUser(u1);
+
+        // View books
+        lib.viewBooks();
+
+        // Issue book
+        lib.issueBook(1, 101);
     }
 }
